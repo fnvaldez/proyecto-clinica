d1d048a0caf253fe58c39b5b0d1ca1879b880e61
fnvaldez <vfnoeli@gmail.com>
2022-01-20 15:09:09 -0300

    primer commit

---
+++ ProyectoClinica/src/main/java/ar/edu/unju/fi/poo/clinica/ProyectoClinicaApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.poo.clinica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoClinicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoClinicaApplication.class, args);
+	}
+
+}
---
+++ ProyectoClinica/src/test/java/ar/edu/unju/fi/poo/clinica/ProyectoClinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.poo.clinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoClinicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
